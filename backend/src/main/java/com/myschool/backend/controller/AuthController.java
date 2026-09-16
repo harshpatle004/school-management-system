daaad33b8af71e85c57af8efd50fcbb96cb99b4f
Harsh <harshpatle004@gmail.com>
2026-09-16 14:20:59 +0530

    updated

--- backend/src/main/java/com/myschool/backend/controller/AuthController.java
+++ backend/src/main/java/com/myschool/backend/controller/AuthController.java
@@ -6,7 +6,7 @@ import com.myschool.backend.service.AuthService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
     private final AuthService authService;
