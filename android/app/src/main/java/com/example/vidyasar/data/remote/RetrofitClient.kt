04a6a75bbc835453c3df7843ea1ae06ce829e671
Harsh <harshpatle004@gmail.com>
2026-09-16 15:02:44 +0530

    push fi

--- android/app/src/main/java/com/example/vidyasar/data/remote/RetrofitClient.kt
+++ android/app/src/main/java/com/example/vidyasar/data/remote/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClient {
 
     private const val BASE_URL =
-        "https://school-management-system-39h4.onrender.com"
+        "https://school-management-system-39h4.onrender.com/"
 
     val authApi = Retrofit.Builder()
         .baseUrl(BASE_URL)
--- android/app/src/main/java/com/example/vidyasar/data/remote/api/AuthApi.kt
+++ android/app/src/main/java/com/example/vidyasar/data/remote/api/AuthApi.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 interface AuthApi {
 
 
-    @POST("/api/auth/login")
+    @POST("api/auth/login")
     suspend fun login(
         @Body request : LoginRequest
     ): Response<LoginResponse>
